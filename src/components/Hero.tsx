90bb16d511eb9202a9d55f36986b75b81cab8592
Natalia Duarte <Naty@MacBook-Pro-de-Ale.local>
2026-02-19 10:45:53 -0300

    Refine Hero: lowercase 'is' in title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8, delay: 0.1, ease: [0.16, 1, 0.3, 1] }}
                 >
-                    TransparentBusiness<br />Is Evolving
+                    TransparentBusiness<br />is Evolving
                 </motion.h1>
 
                 <motion.div
